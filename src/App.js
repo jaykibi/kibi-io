454e4b51217f3ea657f597ae37cf7c2c6b8bac01
jaykibi <raquibbi@gmail.com>
2020-07-02 00:57:39 -0500

    fixed icons, added gh and linkin links

--- src/App.js
+++ src/App.js
@@ -205,17 +205,12 @@ function App() {
     <div class="container">
       <ul class="list-inline mb-5">
         <li class="list-inline-item">
-          <a class="social-link rounded-circle text-white mr-3" href="#!">
-            <i class="icon-social-facebook"></i>
+          <a class="social-link rounded-circle text-white mr-3" href="https://www.linkedin.com/in/raquibbi/">
+            <i class="icon-social-linkedin"></i>
           </a>
         </li>
         <li class="list-inline-item">
-          <a class="social-link rounded-circle text-white mr-3" href="#!">
-            <i class="icon-social-twitter"></i>
-          </a>
-        </li>
-        <li class="list-inline-item">
-          <a class="social-link rounded-circle text-white" href="#!">
+          <a class="social-link rounded-circle text-white" href="https://github.com/jaykibi">
             <i class="icon-social-github"></i>
           </a>
         </li>
