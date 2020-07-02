c899a4d7da11e9b2c8648904b643da2ae8fc3de6
jaykibi <raquibbi@gmail.com>
2020-07-01 23:34:10 -0500

    Updates

---
+++ precache-manifest.8c164ead0631bf5661473f022be469f2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ef7190d8b2e57e5890622f6ddc1cd30",
+    "url": "/jaykibi.github.io/index.html"
+  },
+  {
+    "revision": "f126bade3837f091d18c",
+    "url": "/jaykibi.github.io/static/css/main.f968b230.chunk.css"
+  },
+  {
+    "revision": "c2dd7132f274f50cb70b",
+    "url": "/jaykibi.github.io/static/js/2.696b4b76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jaykibi.github.io/static/js/2.696b4b76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f126bade3837f091d18c",
+    "url": "/jaykibi.github.io/static/js/main.15fb7331.chunk.js"
+  },
+  {
+    "revision": "071dae37dbfe1d2648ab",
+    "url": "/jaykibi.github.io/static/js/runtime-main.8f518f40.js"
+  },
+  {
+    "revision": "31f926ac134297d62f4ef885ff83c526",
+    "url": "/jaykibi.github.io/static/media/bg-callout.31f926ac.jpg"
+  },
+  {
+    "revision": "e2bc0d08898f88bf50d019dd73e51063",
+    "url": "/jaykibi.github.io/static/media/bg-kibi2.e2bc0d08.jpg"
+  }
+]);
