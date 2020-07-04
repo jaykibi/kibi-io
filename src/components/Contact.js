8327ca82a126ec523d67a75629a91b686d08ebbf
jaykibi <raquibbi@gmail.com>
2020-07-03 21:00:55 -0500

    contact form should work now

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,36 @@
 import React from 'react';
 
 class Contact extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.submitForm = this.submitForm.bind(this);
+        this.state = {
+          status: ""
+        };
+    }
+
+    submitForm(ev) {
+        ev.preventDefault();
+        const form = ev.target;
+        const data = new FormData(form);
+        const xhr = new XMLHttpRequest();
+        xhr.open(form.method, form.action);
+        xhr.setRequestHeader("Accept", "application/json");
+        xhr.onreadystatechange = () => {
+          if (xhr.readyState !== XMLHttpRequest.DONE) return;
+          if (xhr.status === 200) {
+            form.reset();
+            this.setState({ status: "SUCCESS" });
+          } else {
+            this.setState({ status: "ERROR" });
+          }
+        };
+        xhr.send(data);
+      }
+
     render() {
+        const { status } = this.state;
         return (
             <section class="mb-5">
                 <h2 class="h1-responsive font-weight-bold text-center my-4">Contact Me</h2>
@@ -11,7 +40,11 @@ class Contact extends React.Component {
                 <div class="row">
 
                     <div class="col-md-6 offset-3 mb-md-0 mb-5">
-                        <form id="contact-form" name="contact-form" action="mail.php" method="POST">
+                        <form
+                            onSubmit={this.submitForm}
+                            action="https://formspree.io/xeqrreyp"
+                            method="POST"
+                        >
 
                             <div class="row">
                                 
@@ -51,12 +84,15 @@ class Contact extends React.Component {
 
                                 </div>
                             </div>
-                        </form>
-
+                        
                         <div class="text-center text-md-right">
-                            <a class="btn btn-primary" onclick="document.getElementById('contact-form').submit();">Send</a>
+                            {status === "SUCCESS" ? <p>Thanks!</p> : <button class="btn btn-primary">Submit</button>}
+                            {status === "ERROR" && <p>Ooops! There was an error.</p>}
                         </div>
-                        <div class="status"></div>
+                        
+
+                        
+                        </form>
                     </div>
 
                 </div>
